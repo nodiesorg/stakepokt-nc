ddf7e88e90d69ea583bda9d4703f93b1fffb7ebe
Ryan Ebro <ryan.ebro@yahoo.com>
2023-03-25 10:03:52 -0400

    pass app data to staketable component, fix dynamic heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,129 +1,138 @@
+import { useState } from "react";
+
 import Head from "next/head";
 import {
-    Box,
-    Button,
-    Card,
-    CardBody,
-    CardHeader,
-    Container,
-    Flex,
-    Heading,
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Container,
+  Flex,
+  Heading,
 } from "@chakra-ui/react";
-import {Step, Steps, useSteps} from "chakra-ui-steps";
-import {ArrowBackIcon} from "@chakra-ui/icons";
+import { Step, Steps, useSteps } from "chakra-ui-steps";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+
 import ImportNcWalletStep from "@/components/stake-steps/import-nc-wallet-step";
 import ImportNodeKeysStep from "@/components/stake-steps/import-node-keys-step";
 import SetStakeAmountStep from "@/components/stake-steps/set-stake-amount-step";
-import StakeTable from "@/components/stake-table/StakeTable";
-import {useEffect, useState} from "react";
+import StakeTable from "@/components/stake-table/stake-table";
 
 // Content for each step
 const steps = [
-    {
-        label: "",
-        headerTitle: "Non Custodial Staking Tool",
-        content: <ImportNcWalletStep/>,
-    },
-    {
-        label: "",
-        headerTitle: "Import Node Keys",
-        content: <ImportNodeKeysStep/>,
-    },
-    {
-        label: "",
-        headerTitle: "Set Stake Distribution",
-        content: <SetStakeAmountStep/>,
-    },
+  {
+    headerTitle: "Non Custodial Staking Tool",
+    content: <ImportNcWalletStep />,
+  },
+  {
+    headerTitle: "Import Node Keys",
+    content: <ImportNodeKeysStep />,
+  },
+  {
+    headerTitle: "Set Stake Distribution",
+    content: <SetStakeAmountStep />,
+  },
 ];
 
 export default function Home() {
+  const { nextStep, prevStep, activeStep } = useSteps({
+    initialStep: 0,
+  });
 
-    const [headerTitle, setActiveStepTitle] = useState(steps[0].headerTitle)
+  // passphrase
+  const [passphrase, setPassphrase] = useState("");
+  // keyfile
+  const [keyfile, setKeyFile] = useState<null | any>();
+  // csv
+  const [csv, setCsv] = useState<null | any>();
+  // stakeamount
+  const [stakeAmount, setStakeAmount] = useState("");
+  // node balance
+  const [nodeBalance, setNodeBalance] = useState("");
+  // non custodial address
+  const [custodialAddress, setNonCustodialAddress] = useState("");
 
-    const {nextStep, prevStep, activeStep} = useSteps({
-        initialStep: 0,
-    });
+  return (
+    <>
+      <Head>
+        <title>Nodies</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    useEffect(() => {
-        if (steps.length >= activeStep)
-            return;
-        setActiveStepTitle(steps[activeStep].headerTitle)
-    }, [activeStep])
+      <Box as="main" minHeight="100vh">
+        <Flex alignItems="center" minHeight="100vh">
+          <Container maxWidth="3xl" height="100%">
+            {/* Multistep card */}
+            <Card backgroundColor="#1B1E30">
+              <CardBody>
+                <CardHeader>
+                  <Heading
+                    color="white"
+                    fontFamily="Poppins"
+                    fontSize="24px"
+                    fontWeight={500}
+                    textAlign="center"
+                  >
+                    {steps[activeStep].headerTitle}
+                  </Heading>
+                </CardHeader>
 
-    return (
-        <>
-            <Head>
-                <title>Nodies</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+                {activeStep !== steps.length ? (
+                  <>
+                    {/* Multistep */}
+                    <Steps
+                      activeStep={activeStep}
+                      orientation="horizontal"
+                      margin="3rem 0"
+                    >
+                      {steps.map(({ content }, index) => (
+                        <Step key={index}>{content}</Step>
+                      ))}
+                    </Steps>
 
-            <Box as="main" minHeight="100vh">
-                <Flex alignItems="center" minHeight="100vh">
-                    <Container maxWidth="3xl" height="100%">
-                        {/* Multistep card */}
-                        <Card backgroundColor="#1B1E30">
-                            <CardBody>
-                                <CardHeader>
-                                    <Heading
-                                        color="white"
-                                        fontFamily="Poppins"
-                                        fontSize="24px"
-                                        fontWeight={500}
-                                        textAlign="center"
-                                    >
-                                        {headerTitle}
-                                    </Heading>
-                                </CardHeader>
-
-                                {activeStep !== steps.length ? (
-                                    <>
-                                        {/* Multistep */}
-                                        <Steps
-                                            activeStep={activeStep}
-                                            orientation="horizontal"
-                                            margin="3rem 0"
-                                        >
-                                            {steps.map(({label, content}, index) => (
-                                                <Step label={label} key={index}>
-                                                    {content}
-                                                </Step>
-                                            ))}
-                                        </Steps>
-
-                                        {/* Next and Back buttons */}
-                                        <Flex width="100%" justify="flex-end">
-                                            <Button
-                                                border="1px solid white"
-                                                color="white"
-                                                isDisabled={activeStep === 0}
-                                                leftIcon={<ArrowBackIcon/>}
-                                                marginRight={4}
-                                                onClick={prevStep}
-                                                size="lg"
-                                                variant="outline"
-                                                _hover={{backgroundColor: "transparent"}}
-                                            >
-                                                Back
-                                            </Button>
-                                            <Button
-                                                backgroundColor="#5C58FF"
-                                                onClick={nextStep}
-                                                size="lg"
-                                                _hover={{backgroundColor: "#5C58FF"}}
-                                            >
-                                                {activeStep === 2 ? "Start staking!" : "Next"}
-                                            </Button>
-                                        </Flex>
-                                    </>
-                                ) : (
-                                    <StakeTable/>
-                                )}
-                            </CardBody>
-                        </Card>
-                    </Container>
-                </Flex>
-            </Box>
-        </>
-    );
+                    {/* Next and Back buttons */}
+                    <Flex width="100%" justify="flex-end">
+                      <Button
+                        border="1px solid white"
+                        color="white"
+                        isDisabled={activeStep === 0}
+                        leftIcon={<ArrowBackIcon />}
+                        marginRight={4}
+                        onClick={prevStep}
+                        size="lg"
+                        variant="outline"
+                        _hover={{ backgroundColor: "transparent" }}
+                      >
+                        Back
+                      </Button>
+                      <Button
+                        backgroundColor="#5C58FF"
+                        onClick={nextStep}
+                        size="lg"
+                        _hover={{ backgroundColor: "#5C58FF" }}
+                      >
+                        {activeStep === 2 ? "Start staking!" : "Next"}
+                      </Button>
+                    </Flex>
+                  </>
+                ) : (
+                  // Pass app data to stake table component
+                  <StakeTable
+                    passphrase={passphrase}
+                    keyfile={keyfile}
+                    csv={csv}
+                    stakeAmount={stakeAmount}
+                    nodeBalance={nodeBalance}
+                    custodialAddress={custodialAddress}
+                  />
+                )}
+              </CardBody>
+            </Card>
+          </Container>
+        </Flex>
+      </Box>
+    </>
+  );
 }
