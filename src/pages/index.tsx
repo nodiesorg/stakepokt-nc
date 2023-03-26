d1da311c534bbad963075d569a444cc97f1b1761
poktblade <poktblade@poktfund.com>
2023-03-26 18:12:39 -0500

    update header title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Nodies</title>
+        <title>Nodies - Non Custodial Staking Tool</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
