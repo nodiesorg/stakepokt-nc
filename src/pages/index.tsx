7f3e81b08c50fe764a2e7d7b72e073da1fcefa20
r2ditto <r2ditto@nodies.org>
2023-03-30 11:25:27 -0400

    update favicon and meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,8 +65,9 @@ export default function Home() {
     <>
       <Head>
         <title>Nodies</title>
+        <meta name="description" content="insert description here" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <Box as="main" minHeight="100vh">
