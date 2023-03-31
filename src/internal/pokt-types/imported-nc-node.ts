cbb201dfa1dea75c91aa1b0210a7a4fba42e9682
poktblade <poktblade@poktfund.com>
2023-03-30 20:36:54 -0500

    adjust form enum

--- src/internal/pokt-types/imported-nc-node.ts
+++ src/internal/pokt-types/imported-nc-node.ts
@@ -1,7 +1,20 @@
+import {TxMsg} from "@/internal/pocket-js-2.1.1/packages/transaction-builder";
+
 export type ImportedNcNode = {
     nodeAlias: string;
     address: string;
     publicKey: string;
     domain?: string;
     chains?: string[];
+}
+
+class StakableNode {
+
+    private node: ImportedNcNode
+    private txMsgs:TxMsg
+    constructor(node: ImportedNcNode, txMsgs:TxMsg) {
+        this.node = node;
+        this.txMsgs = txMsgs
+    }
+
 }
